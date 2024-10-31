0147eac767997fda537c44c3f3cbbd0deb943821
ClaraCarl <clara.carlsson@colorado.edu>
2024-10-30 22:57:54 -0600

    worked on week 10 in class assignment

--- js/ica_10.js
+++ js/ica_10.js
@@ -1,15 +1,25 @@
-let my_var = 100;
-var my_other_var = "hello world";
-const this_is_a_constant = "constant";
-console.log(my_other_var);
-my_other_var += "!";
-//alert(my_other_var)//
+console.log(document.getElementById("my_head")); 
 
-console.log(document.getElementById("my_head"));
-document.getElementById("my_head").addEventListener("click", function(e){
-    alert("clicked the h1");
-    document.getElementById("paragraph").innerHTML = "this is new text";
-    document.getElementById("paragraph").style.color = "blue";
-    document.getElementById("image").src = "images/unicorns/unicorn (2).png";
-})
-alert("this is my alert");
+document.getElementById("bar_taco_header").addEventListener("click", function(e) {
+    alert("clicked the h3");
+    
+=    document.getElementById("paragraph").style.color = "blue";
+
+    let image = document.getElementById("imageUrl");
+    
+    if (!image) {
+        image = document.createElement("p"); 
+        image.id = "imageUrl"; 
+        image.style.color = "blue"; 
+        document.body.appendChild(imageUrlElement); 
+    
+    image.innerHTML = `<a href="https://bartaco.com/location/boulder/" target="_blank">Open the website</a>`;
+});
+
+document.getElementById("restaurant-image").addEventListener("click", function(e) {
+    const description = document.getElementById("description");
+    description.textContent = "Lay-back restaurant with cheap tacos";
+    description.style.display = "block";
+    description.style.float = "right";
+    description.style.marginLeft = "10px";
+});
